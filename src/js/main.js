271e29359ea36d97c8d7826d0f115fcbe66ec706
Pavel <whatisclamp@gmail.com>
2021-11-30 13:16:31 +0200

    Final

--- src/js/main.js
+++ src/js/main.js
@@ -30,4 +30,23 @@ window.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    //special cards hover 
+
+    const specialEach = document.querySelectorAll('.special__offers--each');
+    const circles = document.querySelectorAll('.circle');
+
+    specialEach.forEach((item, i) => {
+        item.addEventListener('mousemove', () => {
+            specialEach[i].classList.add('active');
+            circles.forEach((circle) => circles[i].classList.add('active'))
+        });
+
+        item.addEventListener('mouseleave', () => {
+            specialEach[i].classList.remove('active');
+            circles.forEach((circle) => circles[i].classList.remove('active'))
+        });
+
+    })
+
+
 });
